a2c2fba5fbbef95cab5353ac16434ab282600fd0
Tientjie <tientjieit@gmail.com>
2023-05-13 12:44:48 +0200

    refactor code

--- tournament.py
+++ tournament.py
@@ -14,13 +14,13 @@ AGENTS = [RandomAgent, RandomShortestPathAgent, HumanAgent]
 
 class Tournament:
     def __init__(self, agents: list[Agent]):
-        self.agents = agents
-        self.ranking = Counter({agent: 0 for agent in agents})
+        self._agents: list[Agent] = agents
+        self._ranking: Counter = Counter({agent: 0 for agent in agents})
 
     def run(self):
         # every agent plays every other agent twice
-        for agent_1 in self.agents:
-            for agent_2 in self.agents:
+        for agent_1 in self._agents:
+            for agent_2 in self._agents:
                 if agent_1 == agent_2:
                     continue
                 self.play(agent_1, agent_2)
@@ -38,19 +38,19 @@ class Tournament:
             observation, reward, termination, truncation, info = quoridor_env.last()
             if termination:
                 if quoridor_env.rewards["player_1"] == 1:
-                    self.ranking[agent_1] += 1
+                    self._ranking[agent_1] += 1
                 else:
-                    self.ranking[agent_2] += 1
+                    self._ranking[agent_2] += 1
 
                 break
             action = agents[agent].act(observation, reward, info)
             quoridor_env.step(action)
 
     def get_ranking(self):
-        return self.ranking.most_common()
+        return self._ranking.most_common()
 
 
 if __name__ == "__main__":
     tournament = Tournament(AGENTS)
     tournament.run()
-    print(tournament.ranking)
+    print(tournament.get_ranking())
